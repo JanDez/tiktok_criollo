1fb28c984b54c23c33d24a039e4a02ef0b4b6eae
Jaifran Hernández <programacin@e3creatic.com>
2022-01-12 20:44:07 -0400

    Cleaning the template

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import './App.css'
 
-function App() {
-	const [count, setCount] = useState(0)
-
-	return (
-		<div className='App'>
-				<p>ÇThis is TikTok baby</p>
-		</div>
-	)
+function App () {
+  return (
+    <div className='App'>
+      <p>This is TikTok baby</p>
+    </div>
+  )
 }
 
 export default App
