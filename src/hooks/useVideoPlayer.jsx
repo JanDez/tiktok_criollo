02b2db60c2623add847deebe4d0ae5663852c9c4
Jaifran Hernández <jaifranjose@gmail.com>
2022-01-30 22:00:00 -0400

    fix(Root): Fixing return

--- src/hooks/useVideoPlayer.jsx
+++ src/hooks/useVideoPlayer.jsx
@@ -1,74 +1,75 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react'
 
 const useVideoPlayer = () => {
-  const [player, setPlayerState] = useState(false)
-  const [playing, setPlaying] = useState(false)
+  const [playerState, setPlayerState] = useState(false)
+  const [isPlaying, setisPlaying] = useState(false)
   const [progress, setProgress] = useState(0)
   const [speed, setSpeed] = useState(1)
   const [muted, setMuted] = useState(false)
-}
   const togglePlay = () => {
-		setPlayerState({
-			...playerState,
+    setPlayerState({
+      ...playerState,
 			isPlaying: !playerState.isPlaying,
 		})
   }
-
+  
   useEffect(() => {
-		playerState.isPlaying
-			? videoElement.current.play()
-			: videoElement.current.pause()
+    playerState.isPlaying
+    ? videoElement.current.play()
+    : videoElement.current.pause()
   }, [playerState.isPlaying, videoElement])
-
+  
   const handleOnTimeUpdate = () => {
-		const progress =
-			(videoElement.current.currentTime / videoElement.current.duration) *
-			100
+    const progress =
+    (videoElement.current.currentTime / videoElement.current.duration) *
+    100
 		setPlayerState({
-			...playerState,
+      ...playerState,
 			progress,
 		})
   }
-
+  
   const handleVideoProgress = (event) => {
-		const manualChange = Number(event.target.value)
+    const manualChange = Number(event.target.value)
 		videoElement.current.currentTime =
-			(videoElement.current.duration / 100) * manualChange
+    (videoElement.current.duration / 100) * manualChange
 		setPlayerState({
-			...playerState,
+      ...playerState,
 			progress: manualChange,
 		})
   }
-
+  
   const handleVideoSpeed = (event) => {
-		const speed = Number(event.target.value)
+    const speed = Number(event.target.value)
 		videoElement.current.playbackRate = speed
 		setPlayerState({
-			...playerState,
+      ...playerState,
 			speed,
 		})
   }
-
+  
   const toggleMute = () => {
-		setPlayerState({
-			...playerState,
+    setPlayerState({
+      ...playerState,
 			isMuted: !playerState.isMuted,
 		})
   }
-
-  useEffect(() => {
-		playerState.isMuted
-			? (videoElement.current.muted = true)
-			: (videoElement.current.muted = false)
-  }, [playerState.isMuted, videoElement])
-
-  return () => {
-		playerState,
-		togglePlay,
-		handleOnTimeUpdate,
-		handleVideoProgress,
-		handleVideoSpeed,
-		toggleMute,
-  }
   
-export default useVideoPlayer
+  useEffect(() => {
+    playerState.isMuted
+    ? (videoElement.current.muted = true)
+    : (videoElement.current.muted = false)
+  }, [playerState.isMuted, videoElement])
+  
+  return {
+    playerState,
+    togglePlay,
+    handleOnTimeUpdate,
+    handleVideoProgress,
+    handleVideoSpeed,
+    toggleMute
+  }
+}
+  
+  export default useVideoPlayer
+  
